3793bdebc741a69c11b5d58409190e01c4208ddf
EC2 Default User <ec2-user@ip-172-31-5-109.us-east-2.compute.internal>
2025-07-03 08:37:54 +0000

    fixed the config to finally work

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+
 export default defineConfig({
   plugins: [svelte()],
   server: {
+    port: 80,
     allowedHosts: ['.ngrok-free.app', 'localhost', '127.0.0.1'],
     host: true,
     proxy: {
